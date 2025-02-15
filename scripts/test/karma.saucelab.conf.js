281fd664a4157f24b9c6d4f10e0a9cc55ee221f9
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2025-02-15 17:22:19 +0100

    chore(ci): update safari versions

--- scripts/test/karma.saucelab.conf.js
+++ scripts/test/karma.saucelab.conf.js
@@ -32,32 +32,32 @@ const _ = require('lodash');
 const karmaConf = require('./karma.common.conf');
 
 const browsers = {
-  SL_safari_8: {
+  SL_safari_14: {
     base: 'SauceLabs',
     browserName: 'safari',
-    version: '8.0',
-    platform: 'OS X 10.10',
+    version: '14',
+    platform: 'macOS 11.00',
   },
 
-  SL_safari_9: {
+  SL_safari_15: {
     base: 'SauceLabs',
     browserName: 'safari',
-    version: '9.0',
-    platform: 'OS X 10.11',
+    version: '15',
+    platform: 'macOS 12',
   },
 
-  SL_safari_10: {
+  SL_safari_16: {
     base: 'SauceLabs',
     browserName: 'safari',
-    version: '10.0',
-    platform: 'OS X 10.11',
+    version: '16',
+    platform: 'macOS 12',
   },
 
-  SL_safari_11: {
+  SL_safari_17: {
     base: 'SauceLabs',
     browserName: 'safari',
-    version: '11.0',
-    platform: 'macOS 10.12',
+    version: '17',
+    platform: 'macOS 13',
   },
 
   SL_chrome: {
